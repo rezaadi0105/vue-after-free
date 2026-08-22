1a53ed13079eee5f6817c8a40d7580b5b683e667
rezaadi0105 <rezaadipangestu5@gmail.com>
2026-08-23 03:33:14 +0700

    added version for notif
    Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

--- src/download0/binloader.ts
+++ src/download0/binloader.ts
@@ -557,7 +557,7 @@ export function binloader_init () {
 
           if (closeDelay > 0) {
             log('CONFIG.autoclose enabled - closed after ' + (closeDelay / 1000) + ' seconds...')
-            utils.notify('MOD By REZA, tertutup setelah ' + (closeDelay / 1000) + ' detik...')
+            utils.notify('By REZA v2.0.1, tertutup setelah ' + (closeDelay / 1000) + ' detik...')
             const killId = jsmaf.setInterval(function () {
               jsmaf.clearInterval(killId)
               log('Sending SIGKILL to PID ' + pid_num)
